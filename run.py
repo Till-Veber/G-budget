fa1188621b389eec420d5b1900aee3e7565cdcec
TillVeber <temcikus@gmail.com>
2026-06-01 07:21:01 +0300

    Revert "Testing commit"
    This reverts commit 0ca76f5c3ab6043a769c1b778c9db93f85564b55.

--- run.py
+++ run.py
@@ -1,14 +1,4 @@
-from app import create_app, db
-from app.models import User, Family, Category, Transaction, Invitation, UserPlan, FamilyPlan
-import os
+from app import create_app
 
 app = create_app()
-
-if __name__ == '__main__':
-    # Авто-создание БД при первом запуске (только в debug-режиме)
-    if app.debug and os.environ.get('SKIP_DB_INIT') != '1':
-        with app.app_context():
-            db.create_all()
-            print("🗄️  База данных проверена/создана")
-
-    app.run(debug=True, host='0.0.0.0', port=5000)
+app.run(debug=True)
